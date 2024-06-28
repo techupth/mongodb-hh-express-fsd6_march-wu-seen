c1bc622d920cc4cb7e66f5f014d595d355936aaa
Apirak Fakin <apirakfakin@gmail.com>
2024-06-28 15:50:24 +0700

    feat: add ex-1 and ex-2

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { ObjectId } from "mongodb";
+import { db } from "../utils/db.js";
 
 const productRouter = Router();
 
@@ -6,7 +8,14 @@ productRouter.get("/", (req, res) => {});
 
 productRouter.get("/:id", (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+  const result = await collection.insertOne(req.body);
+  return res.status(200).json({
+    message: "Product has been created successfully",
+    data: { id: result.insertedId },
+  });
+});
 
 productRouter.put("/:id", (req, res) => {});
 
