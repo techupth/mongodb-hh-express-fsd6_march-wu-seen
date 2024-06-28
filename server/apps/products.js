e6cc59241897202a9cb13f2e7d8288b32ee1f889
Apirak Fakin <apirakfakin@gmail.com>
2024-06-28 16:12:26 +0700

    feat: add ex-4

--- server/apps/products.js
+++ server/apps/products.js
@@ -6,14 +6,23 @@ const productRouter = Router();
 const collection = db.collection("products");
 
 productRouter.get("/", async (req, res) => {
-   const result = await collection.find().toArray()
-   return res.status(200).json({
-      message: "success",
-      data: result
-   })
+  const result = await collection.find().toArray();
+  return res.status(200).json({
+    message: "success",
+    data: result,
+  });
 });
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const productId = req.params.id;
+  const objectId = new ObjectId(productId);
+  const result = await collection.findOne({
+    _id: objectId,
+  });
+  return res.json({
+    data: result,
+  });
+});
 
 productRouter.post("/", async (req, res) => {
   const result = await collection.insertOne(req.body);
@@ -23,7 +32,29 @@ productRouter.post("/", async (req, res) => {
   });
 });
 
-productRouter.put("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  const productId = req.params.id;
+  const { name, price, image, description, category } = req.body;
+  const objectId = new ObjectId(productId);
+  const result = await collection.updateOne(
+    {
+      _id: objectId,
+    },
+    {
+      $set: {
+        name: name,
+        price: price,
+        image: image,
+        description: description,
+        category: category,
+      },
+    }
+  );
+  return res.json({
+    message: "Product has been updated successfully",
+    data: result,
+  });
+});
 
 productRouter.delete("/:id", (req, res) => {});
 
