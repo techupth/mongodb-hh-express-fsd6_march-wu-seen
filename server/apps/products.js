24b62c12228edbc872970a7186425070378554fb
Apirak Fakin <apirakfakin@gmail.com>
2024-06-28 15:56:11 +0700

    feat: add ex-3

--- server/apps/products.js
+++ server/apps/products.js
@@ -3,13 +3,19 @@ import { ObjectId } from "mongodb";
 import { db } from "../utils/db.js";
 
 const productRouter = Router();
+const collection = db.collection("products");
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+   const result = await collection.find().toArray()
+   return res.status(200).json({
+      message: "success",
+      data: result
+   })
+});
 
 productRouter.get("/:id", (req, res) => {});
 
 productRouter.post("/", async (req, res) => {
-  const collection = db.collection("products");
   const result = await collection.insertOne(req.body);
   return res.status(200).json({
     message: "Product has been created successfully",
